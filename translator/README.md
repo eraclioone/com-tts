51f5587526fe47a3e468ad2f1d109a2433558382
eraclio <eracliox@outlook.com>
2024-03-25 21:36:54 +0900

    added frontend and server

---
+++ translator/README.md
@@ -0,0 +1,21 @@
+<!-- Improved compatibility of back to top link: See: https://github.com/othneildrew/Best-README-Template/pull/73 -->
+
+<a name="readme-top"></a>
+
+<br />
+<div align="center">
+  <a href="https://github.com/eraclioone/com-tts">
+    <img src="images/logo.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">Speech Assist</h3>
+
+  <p align="center">
+    Speak the world's language with ease – Your voice, translated to connect across borders effortlessly!
+    <br />
+    <a href="https://github.com/eraclioone/com-tts"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://incredible-stardust-1155ac.netlify.app/">View Demo</a>
+  </p>
+</div>
---
+++ translator/src/App.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+
+import translate from "translate";
+import "./App.css";
+import { ChakraProvider, Box, Flex, HStack, VStack, Stack, Button, Container, Image, Heading, Select, Text, Checkbox, Card, CardBody, CardFooter, CardHeader, Divider, Center } from '@chakra-ui/react'
+function App() {
+    const { transcript, resetTranscript } = useSpeechRecognition();
+    const [isListening, setIsListening] = useState(false);
+    const [selectedLanguage, setSelectedLanguage] = useState("en-US");
+    const [transatedText, setTranslatedText] = useState('');
+
+    const [continuous, setContinuous] = useState(true);
+
+    const handleDropdownChange = (event) => {
+        setSelectedLanguage(event.target.value);
+    };
+
+    const handleListening = () => {
+        console.log(continuous)
+        setIsListening(true);
+        SpeechRecognition.abortListening();
+        SpeechRecognition.startListening({
+            continuous: continuous,
+        });
+    };
+
+    // const handleContinuous = (e)=> {
+    //     console.log(continuous)
+    //     setContinuous(e.target.checked);
+    // }
+
+    const handleStop = () => {
+        setIsListening(false);
+        SpeechRecognition.stopListening();
+        console.log("User Input: ", transcript);
+        const languageCode = selectedLanguage.substring(0, 2);
+        translate(transcript, { from: "en", to: languageCode })
+            .then((text) => {
+                console.log("Translated Text: ", text);
+                setTranslatedText(text);
+                const speech = new SpeechSynthesisUtterance();
+                speech.lang = selectedLanguage;
+                speech.text = text;
+                window.speechSynthesis.speak(speech);
+                resetTranscript();
+            })
+            .catch((err) => {
+                console.log(err);
+                resetTranscript();
+            });
+    };
+
+    return (
+        <ChakraProvider>
+            <Box className="App" maxW='full'>
+                <VStack m={10} spacing={10}>
+                    <HStack>
+                        <Image src='/cubes.gif' w={40}></Image>
+                        <Heading>Commune Instant Translator</Heading>
+                    </HStack>
+                    {!SpeechRecognition.browserSupportsSpeechRecognition() && 
+                        <Heading>Browser does not support Speech Recognition.</Heading>
+                    }
+                    <HStack >
+                        <Card align='center'>
+                            <CardHeader>
+                                <Heading size='md'>Select your target language</Heading>
+                            </CardHeader>
+                            <CardBody>
+                                <VStack>
+                                <Select
+                                    borderColor='tomato'
+                                    color='white'
+                                    placeholder='Target language'
+                                    bg='tomato'
+                                    value={selectedLanguage} onChange={handleDropdownChange}
+                                    sx={{'> option': {background: '#2C5282'}}}>
+                                    <option value="en-US" defaultValue>
+                                        English
+                                    </option>
+                                    <option value="es-ES" className="Options">
+                                        Spanish
+                                    </option>
+                                    <option value="fr-FR" className="Options">
+                                        French
+                                    </option>
+                                </Select>
+                                {/* <Checkbox
+                                onChange={handleContinuous}
+                                >Continuous Recognition</Checkbox> */}
+                                <Center height='50px'>
+                                    <Divider orientation='horizontal' />
+                                </Center>
+                                <Center><Text>{transatedText}</Text></Center>
+                                </VStack>
+                            </CardBody>
+                            <CardFooter>
+                                {!isListening &&
+                                    <Button colorScheme='green' onClick={handleListening}>Click to Speak</Button>
+                                }
+                                {isListening &&
+                                    <Button colorScheme='blue' onClick={handleStop}>Listen your voice</Button>
+                                }
+                            </CardFooter>
+                        </Card>
+                    </HStack>
+                </VStack>
+            </Box>
+        </ChakraProvider>
+    );
+}
+
+export default App;
